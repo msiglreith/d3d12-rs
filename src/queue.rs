be8885a052300b876ce945f66561c149ebbf18dc
msiglreith <m.siglreith@gmail.com>
2018-09-16 19:25:48 +0200

    Add command list submission

--- src/queue.rs
+++ src/queue.rs
@@ -1,4 +1,5 @@
 use com::WeakPtr;
+use command_list::CommandList;
 use sync::Fence;
 use winapi::um::d3d12;
 use HRESULT;
@@ -21,5 +22,9 @@ pub type CommandQueue = WeakPtr<d3d12::ID3D12CommandQueue>;
 impl CommandQueue {
     pub fn signal(&self, fence: Fence, value: u64) -> HRESULT {
         unsafe { self.Signal(fence.as_mut_ptr(), value) }
+    }
+
+    pub fn execute_command_lists(&self, command_lists: &[CommandList]) {
+        unsafe { self.ExecuteCommandLists(command_lists.len() as _, command_lists as *const _ as *const *mut _); }
     }
 }
